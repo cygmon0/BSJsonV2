e21e4ca5f4de0e854ade075ca3b6c9b78fe17ce4
abenkdh <32870703+abenkdh@users.noreply.github.com>
2020-05-14 04:18:36 +0800

    ffa
    ffa

--- bsjson/src/main/java/com/benkkstudio/bsjson/Helpers.java
+++ bsjson/src/main/java/com/benkkstudio/bsjson/Helpers.java
@@ -4,8 +4,7 @@ import android.util.Base64;
 
 public class Helpers {
     public static String toBase64(String input) {
-        byte[] encodeValue = Base64.encode(input.getBytes(), Base64.DEFAULT);
-        return new String(encodeValue);
+        return input;
     }
 
 }
